b545446e71f7b5cfd2ad821fcd57e8d817bb81b0
Juan <jsanchez.gr@gmail.com>
2018-02-22 13:30:15 +0100

    First test

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+
+    def test_root_url_resolves_to_homepage(self):
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)
