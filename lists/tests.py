fea39416d71aa50909d16e005db3ce385d209595
Juan <jsanchez.gr@gmail.com>
2018-02-22 16:15:25 +0100

    Saving post and redirection Unit tests

--- lists/tests.py
+++ lists/tests.py
@@ -10,8 +10,8 @@ class HomePageTests(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_post_request(self):
-        response = self.client.post('/', data={'text': 'New'})
-        self.assertIn('New', response.content.decode())
+        response = self.client.post('/', data={'text': 'Brand new item'})
+        self.assertIn('Brand new item', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -27,6 +27,16 @@ class ItemModelTest(TestCase):
 
         item_list = Item.objects.all()
         self.assertEqual(item_list.count(), 2)
-
         self.assertEqual(item_list[0].text, 'This is the first item')
         self.assertEqual(item_list[1].text, 'Item the second')
+
+    def test_save_post_request(self):
+        self.client.post('/', data={'text': 'Brand new item'})
+        self.assertEqual(Item.objects.count(), 1)
+        item = Item.objects.first()
+        self.assertEqual(item.text, 'Brand new item')
+
+    def test_redirect_after_post(self):
+        response = self.client.post('/', data={'text': 'Brand new item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
