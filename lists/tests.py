684d081628f2d4876910eb97847c0efc7529d5be
Juan <jsanchez.gr@gmail.com>
2018-02-22 15:47:19 +0100

    Testing ORM Model

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from lists.models import Item
+
 
 class HomePageTests(TestCase):
 
@@ -10,3 +12,21 @@ class HomePageTests(TestCase):
     def test_can_save_post_request(self):
         response = self.client.post('/', data={'text': 'New'})
         self.assertIn('New', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_save_retrieve_items(self):
+        item_a = Item()
+        item_a.text = 'This is the first item'
+        item_a.save()
+
+        item_b = Item()
+        item_b.text = 'Item the second'
+        item_b.save()
+
+        item_list = Item.objects.all()
+        self.assertEqual(item_list.count(), 2)
+
+        self.assertEqual(item_list[0].text, 'This is the first item')
+        self.assertEqual(item_list[1].text, 'Item the second')
